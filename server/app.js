f073f10f7cd778c5c59c039211fe797978e8aa2c
sandeep-menon <thesandeepmenon@gmail.com>
2021-04-29 13:39:55 +0530

    added MONGO_DB_URI to later ignore the keys file

--- server/app.js
+++ server/app.js
@@ -1,7 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
-const PORT = require("./config/keys").PORT;
+const PORT = process.env.PORT || require("./config/keys").PORT;
+const db = require("./config/keys").MONGO_URI;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true,
+}).then( () => {
+    console.log("Connected to database...");
+}).catch( (err) => {
+    console.log(err);
+})
 
 app.get("/", (req, res) => {
     res.send("hello from server");
--- server/config/keys.js
+++ server/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    PORT: process.env.PORT || 5000,
+    PORT: 5000,
+    MONGO_URI: "MONGO_DB_URI"
 }
